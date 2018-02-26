dd1187012d9d40b5dbf23dc853cc05c5a1084a2f
Glitch (priyanshu-url-shortener) <none>
2018-02-26 21:06:44 +0000

    👧👅 Checkpoint server.js:690280/105

--- server.js
+++ server.js
@@ -8,7 +8,8 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-
+var mongoose=require('mongoose');
+const urlshortner=require('./models/urlshortner');
 if (!process.env.DISABLE_XORIGIN) {
   app.use(function(req, res, next) {
     var allowedOrigins = ['https://narrow-plane.gomix.me', 'https://www.freecodecamp.com'];
