9e5ec085eb16b6996b7332228645acd3a11112d9
Glitch (priyanshu-url-shortener) <none>
2018-03-04 09:05:53 +0000

    🖖😺 Checkpoint server.js:690280/34

--- server.js
+++ server.js
@@ -39,18 +39,18 @@ app.route('/')
     .get(function(req, res) {
 		  res.sendFile(process.cwd() + '/views/index.html');
     })
-app.get('/new/:urlToShorten(*)',function(req,res,next)
+app.get('/new/:urlToShorten(*)',function(req,res)
         {
   var urlToShorten=req.params.urlToShorten;
   var expression=/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi;
-  var eqn
+  var eqn='';
   if(expression.test(urlToShorten)===true)
   {
     eqn= 'Works';
   }
   else
     eqn= 'DoesNOt';
-  
+  res.send(eqn);
   
 });
 
